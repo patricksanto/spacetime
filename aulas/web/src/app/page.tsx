c40086c72e8d039d9456ab484d4e513e2dec8432
Patrick Santos <patrick.sg.santos@gmail.com>
2023-05-16 15:44:05 -0700

    all the fonts and colors of the project imported to the project, in tailswind extends

--- aulas/web/src/app/page.tsx
+++ aulas/web/src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-900 p-6 text-zinc-50">
-      <h1 className="text-4xl font-bold">Sua Capsula do tempo</h1>
+      <h1 className="text-4xl">Sua Capsula do tempo</h1>
+      <h1 className="font-alt text-4xl">Sua Capsula do tempo</h1>
     </div>
   )
 }
