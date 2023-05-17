13b15d1145def55bb3ad74aba303c17cdb6bcd22
Patrick Santos <patrick.sg.santos@gmail.com>
2023-05-17 08:56:57 -0700

    fontes in use in the application, react native now only gonna return anything on screen after the fonts are fully loaded

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -1,7 +1,25 @@
 import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
 export default function App() {
+  const [hasLoadedfonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if(!hasLoadedfonts) {
+    return null
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-gray-900">
       <Text className="text-5xl font-bold text-gray-50 ">Hello World!</Text>
