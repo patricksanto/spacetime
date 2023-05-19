b9047d95a120eb7b09ede9d70fa60e55440cf6e7
Patrick Santos <patrick.sg.santos@gmail.com>
2023-05-19 13:10:38 -0700

    re-do all the code for mobile to fix some issues with react-native

--- aulas/mobile/App.tsx
+++ aulas/mobile/App.tsx
@@ -1,29 +1,11 @@
-import { StatusBar } from 'expo-status-bar'
-import { Text, View } from 'react-native'
-
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from '@expo-google-fonts/roboto'
-
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [hasLoadedfonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold,
-  })
-
-  if(!hasLoadedfonts) {
-    return null
-  }
-
   return (
-    <View className="flex-1 items-center justify-center bg-gray-900">
-      <Text className="text-5xl font-bold text-gray-50 ">Hello World!</Text>
+    <View className='flex-1 bg-gray-700'>
+      <Text className='text-white'>Hello World!</Text>
       <StatusBar style="light" />
     </View>
-  )
+  );
 }
